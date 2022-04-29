5422c2677e571229bb1739086edd1bf24d505948
TheWiseWolfy <bogdan_gabriel757@yahoo.com>
2022-04-29 15:17:18 +0300

    Initial application structure

---
+++ Process-Based-Activty-Manager/Controller/Presenter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Commons;
+
+namespace ActivityTracker
+{
+	public class Presenter : IPresenter
+	{
+	}
+}
---
+++ Process-Based-Activty-Manager/Process-Based-Activty-Manager/View.cs
@@ -0,0 +1,26 @@
+﻿using Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActivityTracker
+{
+	 class View :IView
+	{
+	
+
+
+		[STAThread]
+		void IView.Display()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+
+
+
+		}
+	}
+}
